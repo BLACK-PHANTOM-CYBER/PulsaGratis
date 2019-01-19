773299db957572bdece5304a6a2a870cfdae12e4
BLACK-PHANTOM-CYBER <46845458+BLACK-PHANTOM-CYBER@users.noreply.github.com>
2019-01-19 22:36:37 +0700

    Add files via upload

---
+++ mabok.sh
@@ -0,0 +1,148 @@
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+ungu="\033[35;1m"
+cyan="\033[36;1m"
+white="\033[37;1m"
+red="\033[31;1m"
+clear
+echo $green"              INI ADALAH TOOL UNTUK  MENDAPATKAN PULSA GRATIS
+               $green"             OLEH cLZXCLc DARI TEAM BLACK PHANTOM CYBER
+echo
+echo
+sleep 5
+clear
+toilet -f future " BLACK PHANTOM CYBER " | lolcat
+sleep 5
+clear
+echo $indigo"          GUNAKAN TOOLS INI DENGAN SELAYAKNYA
+             $ungu"            INGAT ALLAH MAHA MELIHAT
+echo
+echo
+sleep 5
+clear 
+figlet " TOOLS OLEH cLZXCLc " | lolcat
+echo $ungu "↑→"$yellow"SELAMAT DATANG DI TOOL KAMI"$cyan"
+===Masukan Nama/Nick Mu sob!!==="$white
+read -p " ↑→$ " kontol
+echo
+echo
+echo
+echo
+echo
+if [ $kontol =  ] || [ $kontol =  ]
+then
+clear
+echo
+echo
+echo
+echo
+echo $red"        JANCOK KAN AKU UDAH BILANG ISI NAMA LU "
+sleep 5
+echo
+echo $red"               NGEYEL SIH KAMU JANCOK"
+sleep 5
+echo
+sleep 5
+echo $red"         MENDING BOLONGIN SABUN LU COLI COK "
+echo
+echo
+echo
+echo
+exit
+echo
+echo
+echo
+echo
+fi
+clear
+echo
+echo
+echo
+echo
+echo $ungu"    HALLO"$yellow" $kontol"$ungu" SI BANGSAD "
+sleep 4
+clear
+echo
+echo
+echo
+echo
+echo $red" ================================================"
+toilet -f slant "BPC" | lolcat
+echo $red" ================================================"
+toilet -f future "cLZXCLc" | lolcat 
+echo $red" ================================================"
+echo
+echo
+echo
+echo
+echo
+echo $yellow"masukin nomor hp lu "
+echo $yellow"dengan awalan +62..."
+echo $yellow"jangan sampai salah"
+read -p "$ " jembut
+echo
+echo
+echo
+echo
+echo
+if [ $jembut =  ] || [ $jembut =  ]
+then
+clear
+echo $cyan"DIBILANGIN MALAH NGEYEL BANGSAD KAU"
+sleep 3
+echo
+echo
+exit
+echo
+echo
+echo
+fi
+echo
+echo
+echo
+echo $yellow"HAI" $indigo" $kontol" $yellow"apakah yakin" $indigo" $jembut" $yellow" ini nomornya?"
+echo $yellow"ketik Y jika iya ketik T jika tidak"
+echo
+echo
+read -p " $ " beha
+echo
+echo
+sleep 5
+clear
+echo $indigo"pilih mau pulsa berapa @all opeeator"
+echo $yellow"1.5k"
+echo $yellow"2.10k"
+echo $yellow"3.20k"
+echo $yellow"4.50k"
+echo $yellow"5.100k"
+echo
+echo
+echo $indigo"masukkan jumlah pulsa yang akan dikirim"
+read -p " $ " susu
+echo
+echo
+sleep 5
+clear
+toilet -f future "HAHAHA " |lolcat
+toilet -f future "KAMU TERTYPU" | lolcat
+toilet -f future "BANGSAD.!!" | lolcat
+echo
+echo
+echo $indigo" $kontol" $green"MAKANYA KALO MAU DAPET PULSA GRATIS KERJA SONO"
+echo $green"BISANYA CARI GRATISAN"
+echo $green"DASAR MANUSIA" $yellow"LEMAH"
+echo $ungu"DADA MANUSIA LEMAH"
+sleep 7
+clear
+echo
+echo
+echo $indigo" BYE-BYE" $yellow" $kontol" $indigo"SI MANUSIA LEMAH"
+echo
+sleep 5
+clear 
+echo
+echo
+toilet -f slant "FUCK" | lolcat
+toilet -f slant "YOU" | lolcat
+toilet -f future "BANGSAD.!" | lolcat
